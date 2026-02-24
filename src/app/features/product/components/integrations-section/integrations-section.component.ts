de37d7142df3237da6ef8a167eef59883592f3bc
praveens2709 <praveen1892293@gmail.com>
2026-02-24 14:33:19 +0530

    feat: Implement dynamic typing animation for chat prompts and display the current day in the integrations section.

--- src/app/features/product/components/integrations-section/integrations-section.component.ts
+++ src/app/features/product/components/integrations-section/integrations-section.component.ts
@@ -8,4 +8,14 @@ import { CommonModule } from '@angular/common';
     templateUrl: './integrations-section.component.html',
     styleUrl: './integrations-section.component.scss'
 })
-export class IntegrationsSectionComponent { }
+export class IntegrationsSectionComponent {
+    currentDayName: string;
+    currentDayNumber: string;
+
+    constructor() {
+        const now = new Date();
+        this.currentDayName = now.toLocaleDateString('de-DE', { weekday: 'long' });
+        this.currentDayName = this.currentDayName.charAt(0).toUpperCase() + this.currentDayName.slice(1);
+        this.currentDayNumber = now.getDate() + '.';
+    }
+}
