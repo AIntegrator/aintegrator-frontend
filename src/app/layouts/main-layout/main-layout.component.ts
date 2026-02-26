4c49af9e4c411b807743f61329aa9d1e3e0c8d19
vanyabrucker <vanyabrucker@gmail.com>
2026-02-26 19:17:11 +0100

    refactor: Remove progress bar component from main layout and update imports accordingly.

--- src/app/layouts/main-layout/main-layout.component.ts
+++ src/app/layouts/main-layout/main-layout.component.ts
@@ -2,12 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from '../../shared/components/navbar/navbar.component';
 import { FooterComponent } from '../../shared/components/footer/footer.component';
-import { ProgressBarComponent } from '../../shared/components/progress-bar/progress-bar.component';
 
 @Component({
     selector: 'app-main-layout',
     standalone: true,
-    imports: [RouterOutlet, NavbarComponent, FooterComponent, ProgressBarComponent],
+    imports: [RouterOutlet, NavbarComponent, FooterComponent],
     templateUrl: './main-layout.component.html',
     styleUrl: './main-layout.component.scss',
 })
