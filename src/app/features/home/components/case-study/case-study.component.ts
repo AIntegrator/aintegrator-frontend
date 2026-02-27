4bab37349843b997574a379a592aba714ef42298
vanyabrucker <vanyabrucker@gmail.com>
2026-02-27 18:29:38 +0100

    feat: Enhance case study component with new content and icon integration

--- src/app/features/home/components/case-study/case-study.component.ts
+++ src/app/features/home/components/case-study/case-study.component.ts
@@ -1,14 +1,17 @@
 import { Component, ChangeDetectionStrategy, ViewEncapsulation } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { LucideAngularModule, Landmark } from 'lucide-angular';
 
 @Component({
     selector: 'app-case-study',
     standalone: true,
-    imports: [CommonModule, RouterModule],
+    imports: [CommonModule, RouterModule, LucideAngularModule],
     templateUrl: './case-study.component.html',
     styleUrl: './case-study.component.scss',
     changeDetection: ChangeDetectionStrategy.OnPush,
     encapsulation: ViewEncapsulation.None
 })
-export class CaseStudyComponent { }
+export class CaseStudyComponent {
+    readonly landmarkIcon = Landmark;
+}
